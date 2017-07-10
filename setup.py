d5963ced007c1ef361264234229294428261bbd3
ksmet1977 <ksmet1977@gmail.com>
2017-07-10 23:02:49 +0200

    update

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
   include_package_data = True,
   classifiers=[
     'Development Status :: 3 - Alpha',
-    'License :: OSI Approved :: GLPv3 License',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 3.5',
     ],  
   python_requires='>=3.5',
