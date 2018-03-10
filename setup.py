577fe57eea412045bb3d7103bab78708d5d1f86c
ksmet1977 <ksmet1977@gmail.com>
2018-03-10 10:55:48 +0100

    update version

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 setup(
   name = 'luxpy',
   packages = find_packages(), 
-  version = '1.1.7',
+  version = '1.1.8',
   license = 'GPLv3',
   description = 'Python lib for lighting and color science',
   author = 'Kevin A.G. Smet',
   author_email = 'ksmet1977@gmail.com',
   url = 'https://github.com/ksmet1977/luxpy',
-  download_url = 'https://github.com/ksmet1977/luxpy/archive/1.1.7.tar.gz',
+  download_url = 'https://github.com/ksmet1977/luxpy/archive/1.1.8.tar.gz',
   keywords = ['color', 'color appearance', 'colorimetry','photometry','CIE','color perception','lighting','color rendering','IES'], 
   install_requires=[
           'numpy',
