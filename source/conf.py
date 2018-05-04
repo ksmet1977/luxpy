02df4e136c0840f06654d26bb331063aeb2240f3
Kevin Smet <kevin.smet@odisee.be>
2018-05-04 02:00:24 +0200

    updateupdate

--- source/conf.py
+++ source/conf.py
@@ -17,14 +17,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
-import mock
-MOCK_MODULES = ['matplotlib.externals']
-for mod_name in MOCK_MODULES:
-	sys.modules[mod_name] = mock.Mock()
+# import mock
+# MOCK_MODULES = ['matplotlib.externals']
+# for mod_name in MOCK_MODULES:
+	# sys.modules[mod_name] = mock.Mock()
 
 # -- General configuration ------------------------------------------------
 
