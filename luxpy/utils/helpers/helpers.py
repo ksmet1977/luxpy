830732d6140c5d7b230370f44419ab3a0713ca78
Kevin Smet <kevin.smet@odisee.be>
2018-06-25 17:35:32 +0200

    added write_to_excel
    Uses openpyxl

--- luxpy/utils/helpers/helpers.py
+++ luxpy/utils/helpers/helpers.py
@@ -57,6 +57,8 @@ Module with helper functions
 
  :todim(): Expand x to dimensions that are broadcast-compatable 
            with shape of another array.
+           
+ :write_to_excel(): | Write a DataFrame to existing an Excel file into specific Sheet.
 
 ===============================================================================
 """
@@ -64,7 +66,7 @@ Module with helper functions
 from luxpy import np, pd, odict, warnings
 __all__ = ['np2d','np3d','np2dT','np3dT','put_args_in_db','vec_to_dict',
            'getdata','dictkv','OD','meshblock','asplit','ajoin',
-           'broadcast_shape','todim']
+           'broadcast_shape','todim','write_to_excel']
 
 #--------------------------------------------------------------------------------------------------
 def np2d(data):
@@ -505,3 +507,79 @@ def todim(x,tshape, add_axis = 1, equal_shape = False):
             return x
         else:
             return np.ones(tshape)*x #make dims of x equal to those of a (tshape)
+        
+#------------------------------------------------------------------------------
+def write_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
+                       truncate_sheet=False, 
+                       **to_excel_kwargs):
+    """
+    Write a DataFrame [df] to existing Excel file [filename]
+    into [sheet_name] Sheet.
+     | If [filename] doesn't exist, then this function will create it.
+
+    Args:
+      :filename: 
+          | File path or existing ExcelWriter
+          | (Example: '/path/to/file.xlsx')
+      :df: 
+          | dataframe to save to workbook
+      :sheet_name: 
+          | Name of sheet which will contain DataFrame.
+          | (default: 'Sheet1')
+      :startrow: 
+          | upper left cell row to dump data frame.
+          | Per default (startrow=None) calculate the last row
+          | in the existing DF and write to the next row...
+      :truncate_sheet: 
+          | truncate (remove and recreate) [sheet_name]
+          | before writing DataFrame to Excel file
+      :to_excel_kwargs: 
+          | arguments which will be passed to `DataFrame.to_excel()`
+          | [can be dictionary]
+
+    Returns: None
+    
+    Notes:
+        Copied from https://stackoverflow.com/questions/20219254/how-to-write-to-an-existing-excel-file-without-overwriting-data-using-pandas
+    """
+    from openpyxl import load_workbook
+
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    writer = pd.ExcelWriter(filename, engine='openpyxl')
+
+    try:
+        # try to open an existing workbook
+        writer.book = load_workbook(filename)
+
+        # get the last row in the existing Excel sheet
+        # if it was not specified explicitly
+        if startrow is None and sheet_name in writer.book.sheetnames:
+            startrow = writer.book[sheet_name].max_row
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+    
+    except FileNotFoundError:
+        # file does not exist yet, we will create it
+        pass
+
+    if startrow is None:
+        startrow = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
+
+    # save the workbook
+    writer.save()
