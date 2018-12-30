bcd12d295b82b6b58e61f6029aa32071ff047838
ksmet1977 <ksmet1977@users.noreply.github.com>
2018-12-30 23:45:23 +0100

    Update circadian_CS_CLa_lrc.py

--- luxpy/toolboxes/photbiochem/circadian_CS_CLa_lrc.py
+++ luxpy/toolboxes/photbiochem/circadian_CS_CLa_lrc.py
@@ -258,7 +258,7 @@ if __name__ == '__main__':
      
     E = 100
     El = _CIE_ILLUMINANTS['D65']
-    S = lx._IESTM30['S']['data'][:4]
+    S = lx._IESTM3015['S']['data'][:4]
     CS, CLa = spd_to_CS_CLa_lrc(El = El, E = E, \
                                     sum_sources = False, interpolate_sources = False)
     print('out')
