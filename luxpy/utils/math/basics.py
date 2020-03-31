a09de6168ca7df7d47891dbdb7c528b272ae00aa
ksmet1977 <ksmet1977@users.noreply.github.com>
2020-03-31 09:49:10 +0200

    Update basics.py

--- luxpy/utils/math/basics.py
+++ luxpy/utils/math/basics.py
@@ -107,7 +107,7 @@ __all__  = ['normalize_3x3_matrix','symmM_to_posdefM','check_symmetric',
             'histogram', 'pol2cart', 'cart2pol', 'spher2cart', 'cart2spher']
 __all__ += ['bvgpdf','mahalanobis2','dot23', 'rms','geomean','polyarea']
 __all__ += ['magnitude_v','angle_v1v2']
-__all__ += ['v_to_cik', 'cik_to_v', 'fmod', 'fit_ellipse','fit_cov_ellipse']
+__all__ += ['v_to_cik', 'cik_to_v', 'fmod', 'remove_outliers','fit_ellipse','fit_cov_ellipse']
 __all__ += ['interp1', 'ndinterp1','ndinterp1_scipy']
 __all__ += ['box_m','pitman_morgan']
 
