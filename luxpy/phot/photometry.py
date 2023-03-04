6af56436d3d849975aec15ae2201c6d2e512f7f3
Kevin A.G. Smet <ksmet1977@users.noreply.github.com>
2023-03-04 21:29:44 +0100

    update

--- luxpy/phot/photometry.py
+++ luxpy/phot/photometry.py
@@ -10,6 +10,8 @@ Module for some photometric conversions and other functions
  :L_to_Er(): convert luminance to retinal illuminance
  
  :Er_to_L(): convert retinal illuminance to luminance
+ 
+ :get_pupil_diameter_watson2012(): Get pupil diameter from adapting luminance, adaptingArea (or Diameter) and observer age
 
 Created on Sat Mar  4 13:38:47 2023
 
@@ -17,7 +19,8 @@ Created on Sat Mar  4 13:38:47 2023
 """
 import numpy as np 
 
-__all__ = ['L_to_td', 'td_to_L', 'L_to_Er', 'Er_to_L']
+__all__ = ['L_to_td', 'td_to_L', 'L_to_Er', 'Er_to_L',
+           'get_pupil_diameter_watson2012']
 
 def L_to_td(L, pa = 1, pd = None):
     """
@@ -118,4 +121,49 @@ def Er_to_L(Er, pa = 1, pd = None, EFL = 16.7, beta = 0):
     return td_to_L(Er*(EFL**2)/np.cos(np.pi/180*beta)**4, pa = pa, pd = pd)
             
 
+#------------------------------------------------------------------------------
+def get_pupil_diameter_watson2012(La, age = 32, adapArea = 10, adapDiam = None, nEyes = 2):
+    """
+    Get pupil diameter from adapting luminance, adaptingArea (or Diameter) and observer age.
+    
+    Args:
+        :La: 
+            | adapting luminance [cd/m²]
+        :adapArea: 
+            | 10, optional
+            | adapting area [degree²]
+        :adapDiam:
+            | None, optional
+            | adapting diameter [degrees]
+            | If None: use adapArea else calculate from adapDiam
+        :nEyes:
+            | 2, optional
+            | Correction for 1 or 2 eye viewing.
+            
+    Returns:
+        :DU:
+            | Pupil diameter in mm.
+            
+    References:
+        1. `Watson, A. & Elliot, J. (2012) "A unified formula for light-adapted pupil size"
+        Journal of Vision (2012) 12(10):12, 1–16 
+        <http://www.journalofvision.org/content/12/10/12>`
+    """
+    Me = 0.1 if nEyes == 1 else 1
+    age = np.atleast_1d(age)
+    # get corneal flux density = product of luminance, area (° squared), and the monocular effect, F=LaM(e)
+    a = adapArea if adapDiam is None else np.pi*(adapDiam/2)**2
+    F = La*a*Me
+    
+    DSD = 7.75 - 5.75*((F/846)**0.41) / (((F/846)**0.41) + 2)
+    S = 0.021323-0.0095623*DSD
+    A = (age - 28.58)*S 
+    DU = DSD + A
+    
+    # simplified forms (with age<20 extension):
+    f = F**0.41
+    DU = (18.5172 + 0.122165*f - 0.105569*age + 0.000138645*f*age) / (2 + 0.0630635*f)
+    DU2 = (16.4674 + np.exp(-0.208269*age*(-3.96868 + 0.00521209 *f)) + 0.124857 * f) / (2 + 0.0630635*f)
+    DU[age<20] = DU2[age<20]
+    return DU
     
